20d81db90ca7568056f500a5c0b4a881979cdc71
Valentin BOUSSON <valentin@bousson.com>
2017-12-08 16:58:50 +0100

    First implementation of Berlekamp-Massey algorithm and full sequence tests

--- lfsr_breaker/analyser.py
+++ lfsr_breaker/analyser.py
@@ -1,12 +1,54 @@
 from .fibonacci_lfsr import FibonacciLFSR
 
-def solve(entry):
-  return 1, 0, 0
+def toInt(v):
+    return 0 if v == [] else v[0] + 2*toInt(v[1:])
+
+def BerlekampMassey(entry):
+    # 1.
+    n = len(entry)
+    s = [int(entry[i]) for i in range(n)]
+
+    # 2.
+    b = [1] + [0]*(n-1)
+    c = [1] + [0]*(n-1)
+
+    # 3.
+    L = 0
+    m = -1
+
+    # 4.
+    for N in range(n):
+        d = sum([c[i]*s[N-i] for i in range(L+1)]) % 2
+
+        if d == 0:
+            #Ok
+            pass
+        else:
+            t = [e for e in c]
+
+            for i in range(n-N+m):
+                c[N-m+i] ^=  b[i]
+
+            if L <= N/2:
+                L = N + 1 - L
+                m = N
+                b = [e for e in t]
+
+    #print(L)
+    if L == 0:
+        L = 1
+    #print(c)
+    #print(b)
+    #print(s)
+    mask = toInt(c[:L][::-1])
+    seed = toInt(s[:L])
+    return L, mask, seed
 
 class LFSRAnalyser:
     def __init__(self, seq):
         self.m_input = seq
-        self.m_res = solve(seq)
+        self.m_res = BerlekampMassey(seq)
+        print("Analyse output : ", self.m_res)
 
     def getInitialFiboLFSR(self):
         L, mask, seed = self.m_res
--- lfsr_breaker/tests/tests_sequencial_analyser.py
+++ lfsr_breaker/tests/tests_sequencial_analyser.py
@@ -1,25 +1,52 @@
 from unittest import TestCase
 
 import lfsr_breaker
+import random
+
+random.seed("toto");
+
+def analyseAndGetNext(seq, expected):
+    print("Input sequence : '{}', expected follow-up : '{}'".format(seq, expected))
+    analyser = lfsr_breaker.LFSRAnalyser(seq)
+    f = analyser.getInitialFiboLFSR()
+    initialSeq = f.generate_n(len(seq), str, "".join)
+    if initialSeq != seq:
+        print("Not valid with command, first seqence generated is : {}".format(initialSeq))
+        return False
+    generated = f.generate_n(len(expected), str, "".join)
+    if expected != generated:
+        print("Wrong guess, generated follow-up : {}".format(generated))
+        return False
+    return True
+
+
+def generateRandomLFSRTestSequence():
+    L = random.randint(1,512)
+    L = 4
+    mask = random.randint(1,2**L-1) | 1;
+    seed = random.randint(1,2**L-1)
+    print (L, mask, seed)
+
+    lfsr = lfsr_breaker.FibonacciLFSR(L, mask, seed);
+    n = random.randint(L*2,L*10)
+    known = lfsr.generate_n(n, str, "".join)
+    m = random.randint(L*100, L*200)
+    expected = lfsr.generate_n(m, str, "".join)
+    return known, expected
 
 class TestSequence(TestCase):
     def testTrivialSequences(self):
-        def analyse_and_get_next(seq, expected):
-            print("Input sequence : '{}', expected follow-up : '{}'".format(seq, expected))
-            analyser = lfsr_breaker.LFSRAnalyser(seq)
-            f = analyser.getInitialFiboLFSR()
-            initialSeq = f.generate_n(len(seq), str, "".join)
-            if initialSeq != seq:
-                print("Not valid with command, first seqence generated is : {}".format(initialSeq))
-            self.assertTrue(initialSeq == seq)
-            generated = f.generate_n(len(expected), str, "".join)
-            if expected != generated:
-                print("Wrong guess, generated follow-up : {}".format(generated))
-            self.assertTrue(expected == generated)
-            print("OK")
-
         big = 10
-        analyse_and_get_next("0", "0"*big)
-        analyse_and_get_next("0"*big, "0"*big)
-        analyse_and_get_next("1"*big, "1"*big)
+        self.assertTrue(analyseAndGetNext("0", "0"*big))
+        self.assertTrue(analyseAndGetNext("0"*big, "0"*big))
+        self.assertTrue(analyseAndGetNext("1"*big, "1"*big))
+
+    def testRandomSequences(self):
+        for i in range(100):
+            print ("Test {} :".format(i+1))
+            known, expected = generateRandomLFSRTestSequence();
+            self.assertTrue(analyseAndGetNext(known, expected))
+
+
+
 
